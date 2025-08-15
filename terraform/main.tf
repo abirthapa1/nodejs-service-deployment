9c61df584d1dfc1bc9ac89ad37075493b3c1bf46
abirthapa1 <abirthapa1@gmail.com>
2025-08-15 12:36:14 -0400

    made some changes to the file structure

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_version = ">=0.15.0, < 2.0.0"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>4.0"
+    }
+  }
+}
+
+
+data "aws_ssm_parameter" "nodejs-ami" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+resource "aws_security_group" "Node-Js-SG" {
+    name = "Node Js SG"
+    description = "Allow HTTP traffic"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "TCP"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "TCP"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+}
+
+resource "aws_key_pair" "nodejs_keypair" {
+  key_name = "Node_Js"
+  public_key = file(var.ssh_key_public)
+}
+
+resource "aws_instance" "Node_Js" {
+    ami = data.aws_ssm_parameter.nodejs-ami.value
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "Node Js server"
+    }
+
+    key_name = aws_key_pair.nodejs_keypair.key_name
+    associate_public_ip_address = true
+
+    #security group configuration
+    vpc_security_group_ids = [ aws_security_group.Node-Js-SG.id ]
+}
